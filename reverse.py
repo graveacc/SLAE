49c30994e475c5421c5d80f7dc192071241fb2db
graveacc <root@osboxes.(none)>
2019-01-23 22:03:12 +0000

    Formatting changes

--- reverse.py
+++ reverse.py
@@ -7,9 +7,9 @@ length = len(code)
 hex_encoded_string = reversed_string.encode('hex')
 split_string = textwrap.wrap(hex_encoded_string, 8)
 
-print 'reversed_string:\n' + reversed_string
-print 'length\n' + str(length)
-print 'hex_encoded_string\n' + hex_encoded_string
-print 'hex encoded string split by 4 bytes\n'
+print '\nreversed_string:\n' + reversed_string
+print '\nlength:\n' + str(length)
+print '\nhex_encoded_string:\n' + hex_encoded_string
+print '\nhex encoded string split by 4 bytes:\n'
 for item in split_string:
 	print '0x'+item
